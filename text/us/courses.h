1e8fff3dfcdc90ed49007bfd9f34e0fc0e628f07
TotorRuns <lpdebora5@gmail.com>
2024-08-13 07:59:37 +0200

    translated some text 100 times with Google Translate

--- text/us/courses.h
+++ text/us/courses.h
@@ -1,61 +1,61 @@
-COURSE_ACTS(COURSE_BOB, _(" 1 BOB-OMB BATTLEFIELD"),
-       _("BIG BOB-OMB ON THE SUMMIT")     , _("FOOTRACE WITH KOOPA THE QUICK") , _("SHOOT TO THE ISLAND IN THE SKY"),
-       _("FIND THE 8 RED COINS")          , _("MARIO WINGS TO THE SKY")        , _("BEHIND CHAIN CHOMP'S GATE"))
+COURSE_ACTS(COURSE_BOB, _(" BOB pla 1 Rad"),
+       _("Laden Sie Shah Busra herunter!")     , _("Diese Klamotten!") , _("Auf geht's zur Badeinsel!"),
+       _("Es besteht aus acht roten Blaettern!")          , _("Neuigkeiten zu allen Blutgruppen!")        , _("Hunde sind kostenlos!"))
 
-COURSE_ACTS(COURSE_WF, _(" 2 WHOMP'S FORTRESS"),
-       _("CHIP OFF WHOMP'S BLOCK")        , _("TO THE TOP OF THE FORTRESS")    , _("SHOOT INTO THE WILD BLUE"),
-       _("RED COINS ON THE FLOATING ISLE"), _("FALL ONTO THE CAGED ISLAND")    , _("BLAST AWAY THE WALL"))
+COURSE_ACTS(COURSE_WF, _(" Unten sind die beiden virtuellen Maschinen."),
+       _("VUMMP ist der Koenig!")        , _("gehen!")    , _("Wir sind gruen!"),
+       _("Es besteht aus acht roten Blaettern!"), _("Ich freue mich auf!")    , _("Schlag die Wand!"))
 
-COURSE_ACTS(COURSE_JRB, _(" 3 JOLLY ROGER BAY"),
-       _("PLUNDER IN THE SUNKEN SHIP")    , _("CAN THE EEL COME OUT TO PLAY?") , _("TREASURE OF THE OCEAN CAVE"),
-       _("RED COINS ON THE SHIP AFLOAT")  , _("BLAST TO THE STONE PILLAR")     , _("THROUGH THE JET STREAM"))
+COURSE_ACTS(COURSE_JRB, _(" 3 Zoll"),
+       _("Der Zug hielt ploetzlich an!")    , _("Sojasauce hinzufuegen!") , _("Dieser Gegenstand befindet sich in einer Höhle!"),
+       _("Es besteht aus acht roten Blaettern!")  , _("Satan lehrt!")     , _("Türstein!"))
 
-COURSE_ACTS(COURSE_CCM, _(" 4 COOL, COOL MOUNTAIN"),
-       _("SLIP SLIDIN' AWAY")             , _("LI'L PENGUIN LOST")             , _("BIG PENGUIN RACE"),
-       _("FROSTY SLIDE FOR 8 RED COINS")  , _("SNOWMAN'S LOST HIS HEAD")       , _("WALL KICKS WILL WORK"))
+COURSE_ACTS(COURSE_CCM, _(" 4 Schrauben der BP-Serie"),
+       _("Ich bin gekommen, um die Vögel zu sehen!")             , _("Wie ein Pinguin!")             , _("Optenoditen greifen schnell an!"),
+       _("Es besteht aus acht roten Blaettern!")  , _("Ich mache Eis!")       , _("Schauen Sie sich die schwarze Wand an!"))
 
-COURSE_ACTS(COURSE_BBH, _(" 5 BIG BOO'S HAUNT"),
-       _("GO ON A GHOST HUNT")            , _("RIDE BIG BOO'S MERRY-GO-ROUND") , _("SECRET OF THE HAUNTED BOOKS"),
-       _("SEEK THE 8 RED COINS")          , _("BIG BOO'S BALCONY")             , _("EYE TO EYE IN THE SECRET ROOM"))
+COURSE_ACTS(COURSE_BBH, _(" 5. Bauen Sie das Schiff zusammen"),
+       _("Lass uns rechnen!")            , _("Genießen Sie Ihre Umgebung!") , _("Entdecken Sie die Geheimnisse der Schule!"),
+       _("Es besteht aus acht roten Blaettern!")          , _("Er lebt in einem großen Haus!")             , _("Finden Sie den Artikel im Supermarkt Simulator!"))
 
-COURSE_ACTS(COURSE_HMC, _(" 6 HAZY MAZE CAVE"),
-       _("SWIMMING BEAST IN THE CAVERN")  , _("ELEVATE FOR 8 RED COINS")       , _("METAL-HEAD MARIO CAN MOVE!"),
-       _("NAVIGATING THE TOXIC MAZE")     , _("A-MAZE-ING EMERGENCY EXIT")     , _("WATCH FOR ROLLING ROCKS"))
+COURSE_ACTS(COURSE_HMC, _(" 6 grüne Pflanzen"),
+       _("Er weiß es!")  , _("Es besteht aus acht roten Blaettern!")       , _("im Meer!"),
+       _("Schau in die Dunkelheit!")     , _("Finden Sie Ihren Weg aus dem Nebel!")     , _("Seien Sie beim Schneiden vorsichtig!"))
 
-COURSE_ACTS(COURSE_LLL, _(" 7 LETHAL LAVA LAND"),
-       _("BOIL THE BIG BULLY")            , _("BULLY THE BULLIES")             , _("8-COIN PUZZLE WITH 15 PIECES"),
-       _("RED-HOT LOG ROLLING")           , _("HOT-FOOT-IT INTO THE VOLCANO")  , _("ELEVATOR TOUR IN THE VOLCANO"))
+COURSE_ACTS(COURSE_LLL, _(" 19:00"),
+       _("Übersetzung!")            , _("Wecker um 19 Uhr... gestellt!")             , _("Es besteht aus acht roten Blaettern!"),
+       _("Information!")           , _("Kennen Sie diesen Berg?")  , _("Auf geht's zum Vulkan!"))
 
-COURSE_ACTS(COURSE_SSL, _(" 8 SHIFTING SAND LAND"),
-       _("IN THE TALONS OF THE BIG BIRD") , _("SHINING ATOP THE PYRAMID")      , _("INSIDE THE ANCIENT PYRAMID"),
-       _("STAND TALL ON THE FOUR PILLARS"), _("FREE FLYING FOR 8 RED COINS")   , _("PYRAMID PUZZLE"))
+COURSE_ACTS(COURSE_SSL, _(" Verwenden Sie Regel 8!"),
+       _("Schauen Sie hinein!") , _("Das ist das Problem!")      , _("Öffne die Pyramide!"),
+       _("Ich habe 4 Kinder!"), _("Es besteht aus acht roten Blaettern!")   , _("Vervollständige das Pyramidenrätsel!"))
 
 COURSE_ACTS(COURSE_DDD, _(" 9 DIRE, DIRE DOCKS"),
-       _("BOARD BOWSER'S SUB")            , _("CHESTS IN THE CURRENT")         , _("POLE-JUMPING FOR RED COINS"),
+       _("BOARD BOWSER'S SUB")            , _("CHESTS IN THE CURRENT")         , _("Es besteht aus acht roten Blaettern.!"),
        _("THROUGH THE JET STREAM")        , _("THE MANTA RAY'S REWARD")        , _("COLLECT THE CAPS..."))
 
 COURSE_ACTS(COURSE_SL, _("10 SNOWMAN'S LAND"),
        _("SNOWMAN'S BIG HEAD")            , _("CHILL WITH THE BULLY")          , _("IN THE DEEP FREEZE"),
-       _("WHIRL FROM THE FREEZING POND")  , _("SHELL SHREDDIN' FOR RED COINS") , _("INTO THE IGLOO"))
+       _("WHIRL FROM THE FREEZING POND")  , _("Es besteht aus acht roten Blaettern!") , _("INTO THE IGLOO"))
 
 COURSE_ACTS(COURSE_WDW, _("11 WET-DRY WORLD"),
        _("SHOCKING ARROW LIFTS!")         , _("TOP O' THE TOWN")               , _("SECRETS IN THE SHALLOWS & SKY"),
-       _("EXPRESS ELEVATOR--HURRY UP!")   , _("GO TO TOWN FOR RED COINS")      , _("QUICK RACE THROUGH DOWNTOWN!"))
+       _("EXPRESS ELEVATOR--HURRY UP!")   , _("Es besteht aus acht roten Blaettern!")      , _("QUICK RACE THROUGH DOWNTOWN!"))
 
 COURSE_ACTS(COURSE_TTM, _("12 TALL, TALL MOUNTAIN"),
-       _("SCALE THE MOUNTAIN")            , _("MYSTERY OF THE MONKEY CAGE")    , _("SCARY 'SHROOMS, RED COINS"),
+       _("SCALE THE MOUNTAIN")            , _("MYSTERY OF THE MONKEY CAGE")    , _("Es besteht aus acht roten Blaettern!"),
        _("MYSTERIOUS MOUNTAINSIDE")       , _("BREATHTAKING VIEW FROM BRIDGE") , _("BLAST TO THE LONELY MUSHROOM"))
 
 COURSE_ACTS(COURSE_THI, _("13 TINY-HUGE ISLAND"),
        _("PLUCK THE PIRANHA FLOWER")      , _("THE TIP TOP OF THE HUGE ISLAND"), _("REMATCH WITH KOOPA THE QUICK"),
-       _("FIVE ITTY BITTY SECRETS")       , _("WIGGLER'S RED COINS")           , _("MAKE WIGGLER SQUIRM"))
+       _("FIVE ITTY BITTY SECRETS")       , _("Es besteht aus acht roten Blaettern!")           , _("MAKE WIGGLER SQUIRM"))
 
 COURSE_ACTS(COURSE_TTC, _("14 TICK TOCK CLOCK"),
        _("ROLL INTO THE CAGE")            , _("THE PIT AND THE PENDULUMS")     , _("GET A HAND"),
-       _("STOMP ON THE THWOMP")           , _("TIMED JUMPS ON MOVING BARS")    , _("STOP TIME FOR RED COINS"))
+       _("STOMP ON THE THWOMP")           , _("TIMED JUMPS ON MOVING BARS")    , _("Es besteht aus acht roten Blaettern!"))
 
 COURSE_ACTS(COURSE_RR, _("15 RAINBOW RIDE"),
-       _("CRUISER CROSSING THE RAINBOW")  , _("THE BIG HOUSE IN THE SKY")      , _("COINS AMASSED IN A MAZE"),
+       _("CRUISER CROSSING THE RAINBOW")  , _("THE BIG HOUSE IN THE SKY")      , _("Es besteht aus acht roten Blaettern!"),
        _("SWINGIN' IN THE BREEZE")        , _("TRICKY TRIANGLES!")             , _("SOMEWHERE OVER THE RAINBOW"))
 
 SECRET_STAR(COURSE_BITDW, _("   BOWSER IN THE DARK WORLD"))
