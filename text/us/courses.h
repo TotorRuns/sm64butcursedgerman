f81118542d4d38023fc197766daa9f9828cea7d5
TotorRuns <74136489+TotorRuns@users.noreply.github.com>
2024-09-13 13:45:13 +0200

    courses.h is translated.

--- text/us/courses.h
+++ text/us/courses.h
@@ -7,71 +7,71 @@ COURSE_ACTS(COURSE_WF, _(" Unten sind die beiden virtuellen Maschinen."),
        _("Es besteht aus acht roten Blaettern!"), _("Ich freue mich auf!")    , _("Schlag die Wand!"))
 
 COURSE_ACTS(COURSE_JRB, _(" 3 Zoll"),
-       _("Der Zug hielt ploetzlich an!")    , _("Sojasauce hinzufuegen!") , _("Dieser Gegenstand befindet sich in einer Höhle!"),
-       _("Es besteht aus acht roten Blaettern!")  , _("Satan lehrt!")     , _("Türstein!"))
+       _("Der Zug hielt ploetzlich an!")    , _("Sojasauce hinzufuegen!") , _("Dieser Gegenstand befindet sich in einer Hoehle!"),
+       _("Es besteht aus acht roten Blaettern!")  , _("Satan lehrt!")     , _("Tuerstein!"))
 
 COURSE_ACTS(COURSE_CCM, _(" 4 Schrauben der BP-Serie"),
-       _("Ich bin gekommen, um die Vögel zu sehen!")             , _("Wie ein Pinguin!")             , _("Optenoditen greifen schnell an!"),
+       _("Ich bin gekommen, um die Voegel zu sehen!")             , _("Wie ein Pinguin!")             , _("Optenoditen greifen schnell an!"),
        _("Es besteht aus acht roten Blaettern!")  , _("Ich mache Eis!")       , _("Schauen Sie sich die schwarze Wand an!"))
 
 COURSE_ACTS(COURSE_BBH, _(" 5. Bauen Sie das Schiff zusammen"),
-       _("Lass uns rechnen!")            , _("Genießen Sie Ihre Umgebung!") , _("Entdecken Sie die Geheimnisse der Schule!"),
-       _("Es besteht aus acht roten Blaettern!")          , _("Er lebt in einem großen Haus!")             , _("Finden Sie den Artikel im Supermarkt Simulator!"))
+       _("Lass uns rechnen!")            , _("Geniessen Sie Ihre Umgebung!") , _("Entdecken Sie die Geheimnisse der Schule!"),
+       _("Es besteht aus acht roten Blaettern!")          , _("Er lebt in einem grossen Haus!")             , _("Finden Sie den Artikel im Supermarkt Simulator!"))
 
-COURSE_ACTS(COURSE_HMC, _(" 6 grüne Pflanzen"),
-       _("Er weiß es!")  , _("Es besteht aus acht roten Blaettern!")       , _("im Meer!"),
+COURSE_ACTS(COURSE_HMC, _(" 6 gruene Pflanzen"),
+       _("Er weiss es!")  , _("Es besteht aus acht roten Blaettern!")       , _("im Meer!"),
        _("Schau in die Dunkelheit!")     , _("Finden Sie Ihren Weg aus dem Nebel!")     , _("Seien Sie beim Schneiden vorsichtig!"))
 
 COURSE_ACTS(COURSE_LLL, _(" 19:00"),
-       _("Übersetzung!")            , _("Wecker um 19 Uhr... gestellt!")             , _("Es besteht aus acht roten Blaettern!"),
+       _("uebersetzung!")            , _("Wecker um 19 Uhr... gestellt!")             , _("Es besteht aus acht roten Blaettern!"),
        _("Information!")           , _("Kennen Sie diesen Berg?")  , _("Auf geht's zum Vulkan!"))
 
 COURSE_ACTS(COURSE_SSL, _(" Verwenden Sie Regel 8!"),
-       _("Schauen Sie hinein!") , _("Das ist das Problem!")      , _("Öffne die Pyramide!"),
-       _("Ich habe 4 Kinder!"), _("Es besteht aus acht roten Blaettern!")   , _("Vervollständige das Pyramidenrätsel!"))
+       _("Schauen Sie hinein!") , _("Das ist das Problem!")      , _("oeffne die Pyramide!"),
+       _("Ich habe 4 Kinder!"), _("Es besteht aus acht roten Blaettern!")   , _("Vervollstaendige das Pyramidenraetsel!"))
 
-COURSE_ACTS(COURSE_DDD, _(" 9 DIRE, DIRE DOCKS"),
-       _("BOARD BOWSER'S SUB")            , _("CHESTS IN THE CURRENT")         , _("Es besteht aus acht roten Blaettern.!"),
-       _("THROUGH THE JET STREAM")        , _("THE MANTA RAY'S REWARD")        , _("COLLECT THE CAPS..."))
+COURSE_ACTS(COURSE_DDD, _(" 9. Durch sauberes Wasser ersetzen!"),
+       _("Bowsers Schiff ist zurueck!")            , _("oeffnen Sie Ihr Portemonnaie!")         , _("Es besteht aus acht roten Blaettern.!"),
+       _("Schwimmen Sie mit Ihren Fuessen!")        , _("Schau dir den Fisch an!")        , _("sei gluecklich!"))
 
-COURSE_ACTS(COURSE_SL, _("10 SNOWMAN'S LAND"),
-       _("SNOWMAN'S BIG HEAD")            , _("CHILL WITH THE BULLY")          , _("IN THE DEEP FREEZE"),
-       _("WHIRL FROM THE FREEZING POND")  , _("Es besteht aus acht roten Blaettern!") , _("INTO THE IGLOO"))
+COURSE_ACTS(COURSE_SL, _("10 Eissorten"),
+       _("Ich moechte leben!")            , _("Es ist ein schwieriges Spiel!")          , _("Lass mich gehen!"),
+       _("diesen Winter!")  , _("Es besteht aus acht roten Blaettern!") , _("ueberpruefen Sie die Nadel!"))
 
-COURSE_ACTS(COURSE_WDW, _("11 WET-DRY WORLD"),
-       _("SHOCKING ARROW LIFTS!")         , _("TOP O' THE TOWN")               , _("SECRETS IN THE SHALLOWS & SKY"),
-       _("EXPRESS ELEVATOR--HURRY UP!")   , _("Es besteht aus acht roten Blaettern!")      , _("QUICK RACE THROUGH DOWNTOWN!"))
+COURSE_ACTS(COURSE_WDW, _("11 Atlantis-aquarium"),
+       _("Schau dir die gruene Ziege an!")         , _("Finden Sie die beliebtesten Orte in Ihrer Stadt!")               , _("Nachschlagen!"),
+       _("er erinnert sich!")   , _("Es besteht aus acht roten Blaettern!")      , _("Zeit zum Nachdenken!"))
 
-COURSE_ACTS(COURSE_TTM, _("12 TALL, TALL MOUNTAIN"),
-       _("SCALE THE MOUNTAIN")            , _("MYSTERY OF THE MONKEY CAGE")    , _("Es besteht aus acht roten Blaettern!"),
-       _("MYSTERIOUS MOUNTAINSIDE")       , _("BREATHTAKING VIEW FROM BRIDGE") , _("BLAST TO THE LONELY MUSHROOM"))
+COURSE_ACTS(COURSE_TTM, _("12 Waffentypen"),
+       _("Ich glaube schon!")            , _("Sehen Sie sich exklusive Videos an!")    , _("Es besteht aus acht roten Blaettern!"),
+       _("Ich schaute auf die Uhr an der Wand!")       , _("Schauen Sie sich die Bruecke an!") , _("Keine grosse Katze!"))
 
-COURSE_ACTS(COURSE_THI, _("13 TINY-HUGE ISLAND"),
-       _("PLUCK THE PIRANHA FLOWER")      , _("THE TIP TOP OF THE HUGE ISLAND"), _("REMATCH WITH KOOPA THE QUICK"),
-       _("FIVE ITTY BITTY SECRETS")       , _("Es besteht aus acht roten Blaettern!")           , _("MAKE WIGGLER SQUIRM"))
+COURSE_ACTS(COURSE_THI, _("Aussage 13"),
+       _("Waehlen Sie ein altes Foto aus!")      , _("Lass uns auf die Insel gehen!"), _("Wir wollen die Welt veraendern!"),
+       _("Loese 5 kleine Raetsel!")       , _("Es besteht aus acht roten Blaettern!")           , _("Schalten Sie das Geraet aus!"))
 
-COURSE_ACTS(COURSE_TTC, _("14 TICK TOCK CLOCK"),
-       _("ROLL INTO THE CAGE")            , _("THE PIT AND THE PENDULUMS")     , _("GET A HAND"),
-       _("STOMP ON THE THWOMP")           , _("TIMED JUMPS ON MOVING BARS")    , _("Es besteht aus acht roten Blaettern!"))
+COURSE_ACTS(COURSE_TTC, _("14 Das ist keine Luege"),
+       _("Wir verkaufen eine breite Produktpalette!")            , _("Schauen Sie sich die Folge an!")     , _("Bring es voran!"),
+       _("Alles ist in Ordnung!")           , _("Nach links!")    , _("Es besteht aus acht roten Blaettern!"))
 
-COURSE_ACTS(COURSE_RR, _("15 RAINBOW RIDE"),
-       _("CRUISER CROSSING THE RAINBOW")  , _("THE BIG HOUSE IN THE SKY")      , _("Es besteht aus acht roten Blaettern!"),
-       _("SWINGIN' IN THE BREEZE")        , _("TRICKY TRIANGLES!")             , _("SOMEWHERE OVER THE RAINBOW"))
+COURSE_ACTS(COURSE_RR, _("15 Franzoesisch schwarz"),
+       _("nach dem Regen!")  , _("Finden Sie Ihr Zuhause im Paradies!")      , _("Es besteht aus acht roten Blaettern!"),
+       _("Der Wind!")        , _("Schauen wir uns das Dreieck an!")             , _("Schau dir das Wasser an!"))
 
-SECRET_STAR(COURSE_BITDW, _("   BOWSER IN THE DARK WORLD"))
-SECRET_STAR(COURSE_BITFS, _("   BOWSER IN THE FIRE SEA"))
-SECRET_STAR(COURSE_BITS, _("   BOWSER IN THE SKY"))
-SECRET_STAR(COURSE_PSS, _("   THE PRINCESS'S SECRET SLIDE"))
-SECRET_STAR(COURSE_COTMC, _("   CAVERN OF THE METAL CAP"))
-SECRET_STAR(COURSE_TOTWC, _("   TOWER OF THE WING CAP"))
-SECRET_STAR(COURSE_VCUTM, _("   VANISH CAP UNDER THE MOAT"))
-SECRET_STAR(COURSE_WMOTR, _("   WING MARIO OVER THE RAINBOW"))
-SECRET_STAR(COURSE_SA, _("   THE SECRET AQUARIUM"))
+SECRET_STAR(COURSE_BITDW, _("   Schatten von Bowser"))
+SECRET_STAR(COURSE_BITFS, _("   Bowser-Lavasee"))
+SECRET_STAR(COURSE_BITS, _("   Lord Bowser ist das Oberhaupt der Familie"))
+SECRET_STAR(COURSE_PSS, _("   Waehlen Sie Ihren Lieblingspilz"))
+SECRET_STAR(COURSE_COTMC, _("   Gruener Tee ist koestlich"))
+SECRET_STAR(COURSE_TOTWC, _("   Das Rote Haus zieht um"))
+SECRET_STAR(COURSE_VCUTM, _("   Der Schluessel ist blau"))
+SECRET_STAR(COURSE_WMOTR, _("   Kalifornien-Projekt, Vereinigte Staaten von Amerika"))
+SECRET_STAR(COURSE_SA, _("   das Wasser des Lebens"))
 SECRET_STAR(COURSE_CAKE_END, _(""))
 
-CASTLE_SECRET_STARS(_("   CASTLE SECRET STARS"))
+CASTLE_SECRET_STARS(_("   Ein grosser Star"))
 
-EXTRA_TEXT(0, _("ONE OF THE CASTLE'S SECRET STARS!"))
+EXTRA_TEXT(0, _("Sie sind in der Kirche!"))
 EXTRA_TEXT(1, _(""))
 EXTRA_TEXT(2, _(""))
 EXTRA_TEXT(3, _(""))
